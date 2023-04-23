149b485a2103bbdee269cf9f478f3476f400aa16
Mónica Galán <mogami.creative@gmail.com>
2023-04-23 10:15:24 +0200

    Initial Commit

---
+++ AnimeApp/AnimeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeAppApp.swift
+//  AnimeApp
+//
+//  Created by Monica Galan de la Llana on 23/4/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimeApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AnimeApp
+//
+//  Created by Monica Galan de la Llana on 23/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
